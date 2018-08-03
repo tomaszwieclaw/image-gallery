eed5d483025ee5339735fc3866a0a88dae07e567
grzegorz.cagara <grzegorz.cagara@app2you.pl>
2018-08-03 08:55:42 +0200

    Implementacja metody validate

--- src/main/java/org/baldogru/imagegallery/service/validator/FileExtensionValidator.java
+++ src/main/java/org/baldogru/imagegallery/service/validator/FileExtensionValidator.java
@@ -1,23 +1,37 @@
 package org.baldogru.imagegallery.service.validator;
 
+import org.apache.commons.io.FilenameUtils;
+import org.baldogru.imagegallery.constants.ValidationStatus;
 import org.baldogru.imagegallery.model.dto.RequestData;
 import org.baldogru.imagegallery.model.dto.ValidationResult;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 final class FileExtensionValidator implements SpecializedValidator {
 
-    FileExtensionValidator() {}
+    FileExtensionValidator() {
+    }
 
     @Value("'${image.allowed.extensions}'.split(',')")
     private List<String> allowedImageExtensions;
 
     @Override
     public ValidationResult validate(RequestData requestData) {
-        //Polecam skorzystac z tego gowna z apache commons-> FilenameUtils.getExtension()
-        return null;
+        String fileName = requestData.getImageFile().getName();
+        String fileExtension = FilenameUtils.getExtension(fileName);
+        Optional<String> validatedFile = allowedImageExtensions.stream().filter(allowedExtension -> allowedExtension.equals(fileExtension)).findFirst();
+        if (validatedFile.isPresent()) {
+            return ValidationResult.builder()
+                    .validationStatus(ValidationStatus.SUCCESS)
+                    .build();
+        }
+        return ValidationResult
+                .builder()
+                .validationStatus(ValidationStatus.FAIL)
+                .build();
     }
 }
