dcd4a309a3b1ad5cd68506770445486de7ed29da
grzegorz.cagara <grzegorz.cagara@app2you.pl>
2018-08-03 08:37:38 +0200

    Implementacja validationSummaryFactory

--- src/main/java/org/baldogru/imagegallery/factory/ValidationSummaryFactoryImpl.java
+++ src/main/java/org/baldogru/imagegallery/factory/ValidationSummaryFactoryImpl.java
@@ -1,19 +1,41 @@
 package org.baldogru.imagegallery.factory;
 
+import org.baldogru.imagegallery.constants.ValidationStatus;
 import org.baldogru.imagegallery.model.dto.ValidationResult;
 import org.baldogru.imagegallery.model.dto.ValidationSummary;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Service
 class ValidationSummaryFactoryImpl implements ValidationSummaryFactory {
 
-    ValidationSummaryFactoryImpl() {}
+    ValidationSummaryFactoryImpl() {
+    }
 
     @Override
     public ValidationSummary create(List<ValidationResult> validationResults) {
-        //Smialo dawaj jakas swoja implementacje tworzenia tego ValidationSummary
-        return null;
+        List<ValidationResult> successValidationResults =
+                validationResults.stream().filter(isValidationResultSuccess()).collect(Collectors.toList());
+        ValidationStatus validationStatus = returnSuccessIfFailValidationResultsIsEmptyElseReturnFail(successValidationResults);
+        return ValidationSummary.builder()
+                .validationResults(validationResults)
+                .validationStatus(validationStatus)
+                .build();
+    }
+
+
+    private Predicate<ValidationResult> isValidationResultSuccess() {
+        return validationResult -> validationResult.getValidationStatus().equals(ValidationStatus.SUCCESS);
+    }
+
+
+    private ValidationStatus returnSuccessIfFailValidationResultsIsEmptyElseReturnFail(List<ValidationResult> successValidationResults) {
+        if (successValidationResults == null || successValidationResults.size() == 0) {
+            return ValidationStatus.SUCCESS;
+        }
+        return ValidationStatus.FAIL;
     }
 }
